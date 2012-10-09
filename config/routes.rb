8d768715449b1cf447fc2ced5236940dac757683
William Lee <me@william-lee.com>
2012-10-09 21:12:16 +0100

    create static_pages controller with home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Superiphase3ds::Application.routes.draw do
+  get "static_pages/home"
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home" do
+    
+    it "should have title Super iPhase 3DS" do
+    	visit '/home/'
+    end
+  end
+end
