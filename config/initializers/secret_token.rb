bcc8f1906346713619ed403aec1e8def524cdc5b
William Lee <me@william-lee.com>
2012-10-09 20:44:22 +0100

    added rails site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superiphase3ds::Application.config.secret_token = '50e896d0c6d504296be70b8c59140097cd0147b40cbeee6d10eee6f301ac6a6cf08919535ebd3db67780865576f65e7fd6852792372e8c63502f04d3021e036c'
